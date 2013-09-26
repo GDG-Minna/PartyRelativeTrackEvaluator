8e6e0911c2c27b7e4b741ff95913aa1068a8f093
Blundell <blundell.app.dev@gmail.com>
2013-09-27 00:28:05 +0100

    removed demo code

--- src/com/blundell/prte/MainActivity.java
+++ src/com/blundell/prte/MainActivity.java
@@ -2,11 +2,7 @@ package com.blundell.prte;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-
-import com.facebook.android.Facebook;
-import com.parse.ParseObject;
 
 public class MainActivity extends PrteActivity {
 
@@ -14,12 +10,6 @@ public class MainActivity extends PrteActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
-
-        Log.d("MainActivity", Facebook.TOKEN);
     }
 
     public void onSignInWithFacebookClick(View button) {
