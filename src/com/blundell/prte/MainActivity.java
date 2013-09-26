45d55a6a29998b4b0fd8d3a2e7a0d6bc8f804dc0
Blundell <blundell.app.dev@gmail.com>
2013-09-26 19:31:31 +0100

    hooked up activity nav

--- src/com/blundell/prte/MainActivity.java
+++ src/com/blundell/prte/MainActivity.java
@@ -1,15 +1,21 @@
 package com.blundell.prte;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
     }
+
+    public void onSignInWithFacebookClick(View button) {
+        Intent intent = new Intent(this, SignUpWithFacebookActiivty.class);
+        startActivity(intent);
+    }
+
 }
---
+++ src/com/blundell/prte/SignUpWithFacebookActiivty.java
@@ -0,0 +1,13 @@
+package com.blundell.prte;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpWithFacebookActiivty extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_with_facebook);
+    }
+}
