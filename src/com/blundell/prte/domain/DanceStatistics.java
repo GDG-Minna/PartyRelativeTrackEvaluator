96e1849099c5b8b92aeff65ef40ab24e16a07a4c
Blundell <blundell.app.dev@gmail.com>
2013-09-27 04:17:59 +0100

    Built the object I would like to have

--- src/com/blundell/prte/domain/DanceStatistics.java
+++ src/com/blundell/prte/domain/DanceStatistics.java
@@ -1,4 +1,31 @@
 package com.blundell.prte.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DanceStatistics {
+    User user;
+    Map<Song, Statistics> statisticsMap = new HashMap<Song, Statistics>();
+
+    private class Statistics {
+        long lengthDancedFor;
+        int caloriesBurned;
+        double intensity;
+    }
+
+    public boolean hasDancedMore(Song song, DanceStatistics competitor) {
+        return getStatistics(song).lengthDancedFor > competitor.getStatistics(song).lengthDancedFor;
+    }
+
+    public boolean hasBurntMore(Song song, DanceStatistics competitor) {
+        return getStatistics(song).caloriesBurned > competitor.getStatistics(song).caloriesBurned;
+    }
+
+    public boolean hasDancedHarder(Song song, DanceStatistics competitor) {
+        return getStatistics(song).intensity > competitor.getStatistics(song).intensity;
+    }
+
+    private Statistics getStatistics(Song song) {
+        return statisticsMap.get(song);
+    }
 }
